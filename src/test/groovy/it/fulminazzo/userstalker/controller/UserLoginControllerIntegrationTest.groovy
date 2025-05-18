4f8a5a8d55ea2475b6f2b5231c772510beb5fe95
user <fulminazzo21@gmail.com>
2025-05-18 21:11:14 +0200

    Created UserLoginController test suite using UserLoginServiceImplIntegrationTest as template

---
+++ src/test/groovy/it/fulminazzo/userstalker/controller/UserLoginControllerIntegrationTest.groovy
@@ -0,0 +1,158 @@
+package it.fulminazzo.userstalker.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import it.fulminazzo.userstalker.repository.UserLoginRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+import static it.fulminazzo.userstalker.service.impl.UserLoginUtils.*
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+class UserLoginControllerIntegrationTest extends Specification {
+
+    @Autowired
+    private UserLoginRepository repository
+
+    @Autowired
+    private MockMvc mockMvc
+
+    private ObjectMapper objectMapper
+
+    @Autowired
+    private UserLoginController controller
+
+    void setup() {
+        setupRepository(repository)
+
+        objectMapper = new ObjectMapper()
+    }
+
+    def 'test that addNewUserLogin adds user'() {
+//        given:
+//        def userLoginDto = UserLoginDto.builder()
+//                .username('fulminazzo')
+//                .ip('99.888.777.66')
+//                .loginDate(LocalDateTime.of(2025, 5, 18, 19, 00, 25))
+//                .build()
+//
+//        when:
+//        def returnedDto = service.addNewUserLogin(userLoginDto)
+//        def entity = repository.findAll().find { it.ip == userLoginDto.ip }
+//
+//        then:
+//        returnedDto.username == userLoginDto.username
+//        returnedDto.ip == userLoginDto.ip
+//        returnedDto.loginDate == userLoginDto.loginDate
+//        entity != null
+//        entity.username == userLoginDto.username
+//        entity.ip == userLoginDto.ip
+//        entity.loginDate == userLoginDto.loginDate
+    }
+
+    def 'test getTopUserLogins returns ordered list with size #size'() {
+//        when:
+//        def logins = service.getTopUserLogins(size)
+//
+//        then:
+//        logins == expected
+//
+//        where:
+//        size || expected
+//        0    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 3),
+//                new UserLoginCountDto(FIRST_USER2.username, 2),
+//                new UserLoginCountDto(FIRST_USER3.username, 1)
+//        ]
+//        1    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 3)
+//        ]
+//        2    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 3),
+//                new UserLoginCountDto(FIRST_USER2.username, 2)
+//        ]
+//        3    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 3),
+//                new UserLoginCountDto(FIRST_USER2.username, 2),
+//                new UserLoginCountDto(FIRST_USER3.username, 1)
+//        ]
+//        4    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 3),
+//                new UserLoginCountDto(FIRST_USER2.username, 2),
+//                new UserLoginCountDto(FIRST_USER3.username, 1)
+//        ]
+    }
+
+    def 'test getTopMonthlyUserLogins returns ordered list with size #size'() {
+//        when:
+//        def logins = service.getTopMonthlyUserLogins(size)
+//
+//        then:
+//        logins == expected
+//
+//        where:
+//        size || expected
+//        0    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 2),
+//                new UserLoginCountDto(FIRST_USER2.username, 1)
+//        ]
+//        1    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 2)
+//        ]
+//        2    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 2),
+//                new UserLoginCountDto(FIRST_USER2.username, 1)
+//        ]
+//        3    || [
+//                new UserLoginCountDto(FIRST_USER1.username, 2),
+//                new UserLoginCountDto(FIRST_USER2.username, 1)
+//        ]
+    }
+
+    def 'test getNewestUserLogins returns ordered list with size #size'() {
+//        when:
+//        def logins = service.getNewestUserLogins(size)
+//
+//        then:
+//        logins == expected.collect { mapper.entityToDto(it) }
+//
+//        where:
+//        size || expected
+//        0    || [FIRST_USER3, THIRD_USER1, SECOND_USER2, SECOND_USER1, FIRST_USER2, FIRST_USER1]
+//        1    || [FIRST_USER3]
+//        2    || [FIRST_USER3, THIRD_USER1]
+//        3    || [FIRST_USER3, THIRD_USER1, SECOND_USER2]
+//        7    || [FIRST_USER3, THIRD_USER1, SECOND_USER2, SECOND_USER1, FIRST_USER2, FIRST_USER1]
+    }
+
+    def 'test getUsernames returns all distinct usernames'() {
+//        when:
+//        def usernames = service.getUsernames()
+//
+//        then:
+//        usernames.size() == 3
+//        FIRST_USER1.username in usernames
+//        FIRST_USER2.username in usernames
+//        FIRST_USER3.username in usernames
+    }
+
+    def 'test getUserLoginsFromUsername of username #username returns expected list'() {
+//        when:
+//        def logins = service.getUserLoginsFromUsername(username)
+//
+//        then:
+//        logins.containsAll(expected.collect { mapper.entityToDto(it) })
+//
+//        where:
+//        username             || expected
+//        FIRST_USER1.username || [FIRST_USER1, SECOND_USER1, THIRD_USER1]
+//        FIRST_USER2.username || [FIRST_USER2, SECOND_USER2]
+//        FIRST_USER3.username || [FIRST_USER3]
+    }
+
+}
