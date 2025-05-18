1216a9aac4b76d424f6f295071c2f454d24e2f24
user <fulminazzo21@gmail.com>
2025-05-18 14:50:56 +0200

    Created UserLogin entity

---
+++ src/main/java/it/fulminazzo/userstalker/domain/entity/UserLogin.java
@@ -0,0 +1,32 @@
+package it.fulminazzo.userstalker.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity(name = "user_logins")
+public class UserLogin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String ip;
+
+    @Column(nullable = false)
+    private LocalDateTime loginDate;
+
+}
