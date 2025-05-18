c141e4cbb8ee74303030bb0e8231b61360338d88
user <fulminazzo21@gmail.com>
2025-05-18 13:36:23 +0200

    Created SecurityConfig with configuration of SecurityFilterChain

---
+++ src/main/java/it/fulminazzo/userstalker/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package it.fulminazzo.userstalker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/logout", "/css/**", "/scripts/**", "/images/**").permitAll()
+                .anyRequest().authenticated()
+        ).formLogin(form -> form
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+        );
+        return http.build();
+    }
+
+}
