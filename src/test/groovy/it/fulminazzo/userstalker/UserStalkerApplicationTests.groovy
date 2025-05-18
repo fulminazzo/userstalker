d623aa80f36cc143635e60bc4a7b0205de8db5b4
user <fulminazzo21@gmail.com>
2025-05-18 13:22:43 +0200

    Added mapstruct and spock dependencies

---
+++ src/test/groovy/it/fulminazzo/userstalker/UserStalkerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package it.fulminazzo.userstalker
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class UserStalkerApplicationTests extends Specification {
+
+    def 'test context loads'() {
+
+    }
+
+}
