cd8106c4e942e51db91c77a6f23f1e66612d1323
user <fulminazzo21@gmail.com>
2025-05-18 18:38:42 +0200

    Updated UserLoginCount

--- src/main/java/it/fulminazzo/userstalker/domain/dto/UserLoginCount.java
+++ src/main/java/it/fulminazzo/userstalker/domain/dto/UserLoginCount.java
@@ -1,19 +1,8 @@
 package it.fulminazzo.userstalker.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Represents the times a user has logged in given a certain period.
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserLoginCount {
-
-    private String username;
-
-    private int loginCount;
+public record UserLoginCount(String username, long loginCount) {
 
 }
