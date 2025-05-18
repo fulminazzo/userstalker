d49f8473be26e0f44cb4f8c913ce6f8fe3030f4e
user <fulminazzo21@gmail.com>
2025-05-18 19:41:42 +0200

    Updated mock Users with relative dates

--- src/test/groovy/it/fulminazzo/userstalker/service/impl/UserLoginUtils.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/service/impl/UserLoginUtils.groovy
@@ -9,37 +9,48 @@ class UserLoginUtils {
     static final UserLogin FIRST_USER1 = UserLogin.builder()
             .username('fulminazzo')
             .ip('11.222.333.44')
-            .loginDate(LocalDateTime.of(2025, 4, 18, 17, 30))
+            .loginDate(newDateTime(-1, 18, 17, 30))
             .build()
 
     static final UserLogin SECOND_USER1 = UserLogin.builder()
             .username('fulminazzo')
             .ip('11.222.333.44')
-            .loginDate(LocalDateTime.of(2025, 5, 19, 00, 30))
+            .loginDate(newDateTime(0, 19, 00, 30))
             .build()
 
     static final UserLogin THIRD_USER1 = UserLogin.builder()
             .username('fulminazzo')
             .ip('12.232.343.54')
-            .loginDate(LocalDateTime.of(2025, 5, 20, 00, 30))
+            .loginDate(newDateTime(0, 20, 00, 30))
             .build()
 
     static final UserLogin FIRST_USER2 = UserLogin.builder()
             .username('alex')
             .ip('12.223.334.45')
-            .loginDate(LocalDateTime.of(2025, 4, 18, 17, 35))
+            .loginDate(newDateTime(-1, 18, 17, 35))
             .build()
 
     static final UserLogin SECOND_USER2 = UserLogin.builder()
             .username('alex')
             .ip('22.443.564.45')
-            .loginDate(LocalDateTime.of(2025, 5, 19, 00, 35))
+            .loginDate(newDateTime(0, 19, 00, 35))
             .build()
 
     static final UserLogin FIRST_USER3 = UserLogin.builder()
             .username('steve')
             .ip('00.111.000.11')
-            .loginDate(LocalDateTime.of(2025, 6, 1, 12, 35))
+            .loginDate(newDateTime(1, 1, 12, 35))
             .build()
+
+    static LocalDateTime newDateTime(int monthOffset, int dayOfMonth, int hour, int minute) {
+        def now = LocalDateTime.now()
+        def year = now.year
+        def month = now.month.value + monthOffset
+        if (month <= 0) {
+            month += 12
+            year--
+        }
+        return LocalDateTime.of(year, month, dayOfMonth, hour, minute)
+    }
 
 }
