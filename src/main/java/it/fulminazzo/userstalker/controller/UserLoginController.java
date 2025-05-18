afeb7aa5f7a41da21f62c0a8f84485ffb86dc219
user <fulminazzo21@gmail.com>
2025-05-18 21:24:40 +0200

    Added Javadoc

--- src/main/java/it/fulminazzo/userstalker/controller/UserLoginController.java
+++ src/main/java/it/fulminazzo/userstalker/controller/UserLoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The controller responsible for the endpoint: <code>/v1/api/userlogins</code>
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/api/userlogins")
@@ -18,32 +21,68 @@ public class UserLoginController {
 
     private final UserLoginService service;
 
+    /**
+     * Response for a POST request.
+     * Creates a new user login in the database.
+     *
+     * @param userLoginDto the user login dto
+     * @return the user login dto
+     */
     @PostMapping
     public ResponseEntity<?> postUserLogin(@Valid @RequestBody UserLoginDto userLoginDto) {
         userLoginDto = service.addNewUserLogin(userLoginDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(userLoginDto);
     }
 
+    /**
+     * Gets the top user logins per count.
+     *
+     * @param count if specified, the list will be limited to the given amount
+     * @return the user logins
+     */
     @GetMapping("/top")
     public List<UserLoginCountDto> getTopUserLogins(@RequestParam int count) {
         return service.getTopUserLogins(count);
     }
 
+    /**
+     * Gets the top monthly user logins per count.
+     *
+     * @param count if specified, the list will be limited to the given amount
+     * @return the user logins
+     */
     @GetMapping("/month")
     public List<UserLoginCountDto> getMonthlyUserLogins(@RequestParam int count) {
         return service.getTopMonthlyUserLogins(count);
     }
 
+    /**
+     * Gets the newest user logins.
+     *
+     * @param count if specified, the list will be limited to the given amount
+     * @return the newest user logins
+     */
     @GetMapping("/newest")
     public List<UserLoginDto> getNewestUserLogins(@RequestParam int count) {
         return service.getNewestUserLogins(count);
     }
 
+    /**
+     * Gets all the registered usernames.
+     *
+     * @return the usernames
+     */
     @GetMapping("/usernames")
     public List<String> getUsernames() {
         return service.getUsernames();
     }
 
+    /**
+     * Gets all the user logins of the given username.
+     *
+     * @param username the username
+     * @return the user logins
+     */
     @GetMapping("/{username}")
     public List<UserLoginDto> getUserLogins(@PathVariable String username) {
         return service.getUserLoginsFromUsername(username);
