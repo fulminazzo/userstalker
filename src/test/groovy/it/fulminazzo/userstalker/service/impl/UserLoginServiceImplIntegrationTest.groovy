099a29f1b25afee83756f394b41b4eaa95d0dfd6
user <fulminazzo21@gmail.com>
2025-05-18 19:00:08 +0200

    Added integration tests for addNewUserLogin method

--- src/test/groovy/it/fulminazzo/userstalker/service/impl/UserLoginServiceImplIntegrationTest.groovy
+++ src/test/groovy/it/fulminazzo/userstalker/service/impl/UserLoginServiceImplIntegrationTest.groovy
@@ -1,11 +1,14 @@
 package it.fulminazzo.userstalker.service.impl
 
+import it.fulminazzo.userstalker.domain.dto.UserLoginDto
 import it.fulminazzo.userstalker.mapper.UserLoginMapper
 import it.fulminazzo.userstalker.repository.UserLoginRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import spock.lang.Specification
+
+import java.time.LocalDateTime
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -21,6 +24,28 @@ class UserLoginServiceImplIntegrationTest extends Specification {
 
     void setup() {
         service = new UserLoginServiceImpl(repository, mapper)
+    }
+
+    def 'test that addNewUserLogin adds user'() {
+        given:
+        def userLoginDto = UserLoginDto.builder()
+                .username('fulminazzo')
+                .ip('99.888.777.66')
+                .loginDate(LocalDateTime.of(2025, 5, 18, 19, 00, 25))
+                .build()
+
+        when:
+        def returnedDto = service.addNewUserLogin(userLoginDto)
+        def entity = repository.findAll().find { it.ip == userLoginDto.ip }
+
+        then:
+        returnedDto.username == userLoginDto.username
+        returnedDto.ip == userLoginDto.ip
+        returnedDto.loginDate == userLoginDto.loginDate
+        entity != null
+        entity.username == userLoginDto.username
+        entity.ip == userLoginDto.ip
+        entity.loginDate == userLoginDto.loginDate
     }
 
 }
