dc62c6f593333dbc5a30709b7610ae1dd37b68f2
user <fulminazzo21@gmail.com>
2025-05-18 13:19:27 +0200

    Initial commit

---
+++ src/main/java/it/fulminazzo/userstalker/UserStalkerApplication.java
@@ -0,0 +1,13 @@
+package it.fulminazzo.userstalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserStalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/fulminazzo/userstalker/UserStalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.fulminazzo.userstalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
