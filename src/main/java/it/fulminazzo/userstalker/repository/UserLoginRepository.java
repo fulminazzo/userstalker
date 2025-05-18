dff7ef01b9568404af8feb917d68d82c71810efd
user <fulminazzo21@gmail.com>
2025-05-18 17:48:39 +0200

    Added findDistinctUsernames method

--- src/main/java/it/fulminazzo/userstalker/repository/UserLoginRepository.java
+++ src/main/java/it/fulminazzo/userstalker/repository/UserLoginRepository.java
@@ -2,8 +2,10 @@ package it.fulminazzo.userstalker.repository;
 
 import it.fulminazzo.userstalker.domain.entity.UserLogin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -11,5 +13,13 @@ import java.util.UUID;
  */
 @Repository
 public interface UserLoginRepository extends JpaRepository<UserLogin, UUID> {
+
+    /**
+     * Returns a distinct list containing all the usernames of the {@link UserLogin}s.
+     *
+     * @return the list
+     */
+    @Query("SELECT DISTINCT u.username FROM user_logins as u")
+    List<String> findDistinctUsernames();
 
 }
