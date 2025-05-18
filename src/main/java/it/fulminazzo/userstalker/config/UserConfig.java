2efe952e5fc942cb45cf9e4a3d6c9fa8bf956682
user <fulminazzo21@gmail.com>
2025-05-18 13:58:12 +0200

    Created UserConfig class for web authentication

---
+++ src/main/java/it/fulminazzo/userstalker/config/UserConfig.java
@@ -0,0 +1,20 @@
+package it.fulminazzo.userstalker.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Configuration
+@ConfigurationProperties(prefix = "auth")
+class UserConfig {
+
+    private String username;
+
+    private String password;
+
+}
