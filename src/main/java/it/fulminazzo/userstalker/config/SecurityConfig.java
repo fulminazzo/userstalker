33578208a8bb1727b26fdbad991c0bfd7985640e
user <fulminazzo21@gmail.com>
2025-05-18 14:09:49 +0200

    Added UserConfig with default UserDetailsService and PasswordEncoder

--- src/main/java/it/fulminazzo/userstalker/config/SecurityConfig.java
+++ src/main/java/it/fulminazzo/userstalker/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package it.fulminazzo.userstalker.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private UserConfig userConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,10 +27,26 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         ).formLogin(form -> form
                 .loginPage("/login")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/", true)
                 .permitAll()
-        );
+        )
+        ;
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .passwordEncoder(s -> passwordEncoder().encode(s))
+                .username(userConfig.getUsername())
+                .password(userConfig.getPassword())
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
